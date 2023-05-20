f0a989873a77d553572a09dddf055ce5af79a6bc
Taylan Tutar <tylntutar@gmail.com>
2023-05-20 13:42:02 +0300

    groping by reduce

--- arrays/reduce.js
+++ arrays/reduce.js
@@ -13,13 +13,44 @@ const students = [
     },
     {
         name: "Ali",
-        surname: "Kaya",
+        surname: "Ekici",
         age: 20,
         notes: [66, 8, 90]
+    },
+    {
+        name: "Murat",
+        surname: "Demir",
+        age: 18,
+        notes: [10, 80, 40]
     }
 ]
 
-let totalAge = students.reduce((acc , student) => {
-    return acc +  student.age
-},0);
+let totalAge = students.reduce((acc, student) => {
+    return acc + student.age
+}, 0);
 console.log(totalAge);
+console.log("*************");
+
+const sameName = students.reduce((acc, student) => {
+    if (student.name in acc) {
+        acc[student.name] += 1;
+    } else {
+        acc[student.name] = 1;
+    }
+    return acc;
+}, {})
+console.log(sameName)
+console.log("*************");
+
+let key = "age"
+let groupedByAge = students.reduce((acc,student) => {
+    if(acc[student[key]]){
+        acc[student[key]].push(student);
+    }else{
+        acc[student[key]] = [];
+        acc[student[key]].push(student);
+    }
+    return acc;
+},{})
+console.log(groupedByAge);
+
